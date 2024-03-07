0abb07fa29efd9613344bbbe3c57c8c86758d9b6
WILLIAM BP <williambryanp77@gmai.com>
2024-03-07 11:49:28 +0700

    Cohort 6

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.printf("\t/\\\\_//\\ \n"
+                        + "   / 0\t 0 \\ \n"
+                        + "  /_   ^   _\\ \n"
+                        + "\t\\_____/ \n"
+                        + "\t/  0  \\ \n"
+                        + "   /\t   \\_ \n"
+                        + "   \\_(_|_)__ \\ \n"
+                        + "\t\t  (__/");
+        }
+    }
